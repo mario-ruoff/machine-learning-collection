53f756287b73a4e2966ed6eed5f45691520ec888
mario-ruoff <mario.srx99@gmail.com>
2024-05-17 21:39:09 +0200

    Add dataset and dataloader

--- neural_network.py
+++ neural_network.py
@@ -3,9 +3,17 @@
 """An implementation of a simple neural network
 """
 
+import random
+import matplotlib.pyplot as plt
+from data_loader import MnistDataloader
+
 
 def main():
-    print("lol")
+    mnist_dataloader = MnistDataloader()
+    (x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
+    
+    plt.imshow(x_train[1], cmap=plt.cm.gray)
+    plt.show()
 
 if __name__ == '__main__':
     main()
